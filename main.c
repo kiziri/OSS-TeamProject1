7547aaa3ea103b6a81962b0d8d40f2ac266cfcc8
kiziri <58429073+kiziri@users.noreply.github.com>
2020-05-06 11:02:05 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void deco();
+void retry();
+void Palindrome();
+
+int main()
+{
+   deco();
+
+   while (1)
+   {
+      Palindrome();
+      retry();
+   }
+
+   return 0;
+}
+
+
+
+
+
+
