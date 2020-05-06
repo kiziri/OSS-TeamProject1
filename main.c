55566c4cb2d31dc6872deb7ce65be98f6909978b
King-Otaku <53389350+King-Otaku@users.noreply.github.com>
2020-05-06 11:04:43 +0900

    Update new Function
    palindrome function added

--- main.c
+++ main.c
@@ -25,8 +25,24 @@ void deco() {
    printf("\t--------------------------------------------------\n\n\n");
 }
 
+void Palindrome() {
+   int cnt, i, result;
+   char aa[101];
+   cnt = 0;
 
+   printf("문자열을 입력하시오.(최대 100자)\n");
+   gets(aa);
 
+   while (aa[cnt] != 0) cnt++;
 
-
-
+   for (i = 0; i < cnt; i++)
+   {
+      if (aa[i] != aa[cnt - i - 1])
+         break;
+   }
+   if(cnt == i)
+      printf("\n\n이 문자열은 회문(Palindrome)이 맞습니다. \n\n");
+   else
+      printf("\n\n이 문자열은 회문(Palindrome)이 아닙니다. \n\n");
+      
+}
