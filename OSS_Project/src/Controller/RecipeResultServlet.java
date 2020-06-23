f521ad3ae2d6b75588a4bccebb1dd7cd93c6e287
kiziri <wkwlwkwlwh@gmail.com>
2020-06-23 22:31:00 +0900

    Update resulServlet.java and RecipeResultServlet.java

--- OSS_Project/src/Controller/RecipeResultServlet.java
+++ OSS_Project/src/Controller/RecipeResultServlet.java
@@ -7,35 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RecipeResultServlet
- */
-@WebServlet("/RecipeResultServlet")
+@WebServlet("/RecipeResult.do")
 public class RecipeResultServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RecipeResultServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMsg", e.getMessage());
+	        request.getRequestDispatcher("/error.jsp").forward(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
--- OSS_Project/src/Controller/resultServlet.java
+++ OSS_Project/src/Controller/resultServlet.java
@@ -8,26 +8,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/resultServlet")
+@WebServlet("/result.do")
 public class resultServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    
-    public resultServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		try {
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMsg", e.getMessage());
+	        request.getRequestDispatcher("/error.jsp").forward(request, response);
+		}
 	}
 
 }
