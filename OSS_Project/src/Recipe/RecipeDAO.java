51a2f4b6b431bf11e11ad78d062c636806ef44c0
CBK107 <55549629+CBK107@users.noreply.github.com>
2020-06-27 04:10:30 +0900

    Update DAO

--- OSS_Project/src/Recipe/RecipeDAO.java
+++ OSS_Project/src/Recipe/RecipeDAO.java
@@ -1,5 +1,34 @@
 package Recipe;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Recipe.Recipe;
+import util.DBManager;
+
 public class RecipeDAO {
 
+	public ArrayList<Recipe> select() throws Exception {
+		Connection con =null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = 
+				
+		ArrayList<Recipe> list = new ArrayList<Recipe>();
+		
+		try {
+			con = DBManager.getConnection();
+			stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+							}
+		}
+		finally {
+			DBManager.close(rs, stmt, con);
+		}
+		
+		return list;
+	}
 }
--- OSS_Project/src/Taste/TasteDAO.java
+++ OSS_Project/src/Taste/TasteDAO.java
@@ -1,7 +1,35 @@
 package Taste;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import util.DBManager;
+import Taste.Taste;
+
 public class TasteDAO {
+	//맛 선택(조회)??
+	public ArrayList<Taste> select(String TasteName, String TasteID) throws Exception{
+	Connection con =null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
+	String sql =
+			
+	ArrayList<Taste> list = new ArrayList<Taste>();
 	
-	
-	
+	try {
+		con = DBManager.getConnection();
+		stmt = con.prepareStatement(sql);
+		rs = stmt.executeQuery();
+		while (rs.next()) {
+			
+		}
+	}
+	finally {
+		DBManager.close(rs, stmt, con);
+	}
+	return list;
+	}
+
 }
