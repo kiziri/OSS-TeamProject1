50db0cf8351fb367e8b9d062496698d51cacbc29
kiziri <wkwlwkwlwh@gmail.com>
2020-06-26 21:44:56 +0900

    Update Cook.java

--- OSS_Project/src/Cook/Cook.java
+++ OSS_Project/src/Cook/Cook.java
@@ -5,6 +5,14 @@ public class Cook {
 	private int IngredientsID; //식재료 ID(외래키)
 	private int RecipeID; //레시피 ID(외래키)
 	private int Taste_ID; //맛 ID(외래키)
+
+	
+	public Cook(int ingredientsID, int recipeID, int taste_ID) {
+		super();
+		IngredientsID = ingredientsID;
+		RecipeID = recipeID;
+		Taste_ID = taste_ID;
+	}
 	
 	public int getTasteID() {
 		return Taste_ID;
