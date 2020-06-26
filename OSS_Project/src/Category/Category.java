0a1e7b19ae44f70ff21dd173ae5848cd96398c7f
CBK107 <55549629+CBK107@users.noreply.github.com>
2020-06-26 21:45:30 +0900

    Update Category.java

--- OSS_Project/src/Category/Category.java
+++ OSS_Project/src/Category/Category.java
@@ -5,6 +5,12 @@ public class Category {
 	private int CategoryID; //카테고리 ID
 	private String CategoryName; //카테고리 이름(중식, 한식, 일식, 양식)
 	
+	public Category(int categoryID, String categoryName) {
+		super();
+		CategoryID = categoryID;
+		CategoryName = categoryName;
+	}
+	
 	public int getCategoryID() {
 		return CategoryID;
 	}
