2989e2c9e4f53604740af132ed38b8e48982074e
CBK107 <55549629+CBK107@users.noreply.github.com>
2020-06-27 04:09:40 +0900

    Update DAO

--- OSS_Project/src/Category/CategoryDAO.java
+++ OSS_Project/src/Category/CategoryDAO.java
@@ -1,5 +1,35 @@
 package Category;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Category.Category;
+import util.DBManager;
+
 public class CategoryDAO {
 
+	public ArrayList<Category> select() throws Exception {
+		Connection con =null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = 
+				
+		ArrayList<Category> list = new ArrayList<Category>();
+		
+		try {
+			con = DBManager.getConnection();
+			stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				
+			}
+		}
+		finally {
+			DBManager.close(rs, stmt, con);
+		}
+		
+		return list;
+	}
 }
--- OSS_Project/src/Cook/CookDAO.java
+++ OSS_Project/src/Cook/CookDAO.java
@@ -1,5 +1,34 @@
 package Cook;
 
-public class CookDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import Cook.Cook;
+import util.DBManager;
+
+public class CookDAO {
+	//재료, 레시피, 맛 아이디 가져오기(외래키)
+	public ArrayList<Cook> select() throws Exception{
+		Connection con =null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = 
+		ArrayList<Cook> list = new ArrayList<Cook>();
+		
+		try {
+			con = DBManager.getConnection();
+			stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+						}
+		}
+		finally {
+			DBManager.close(rs, stmt, con);
+		}
+		
+		return list;
+		
+	}
 }
