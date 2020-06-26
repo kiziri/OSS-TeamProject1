fbee8702e1fdb3aa4f5c7cebb6c33a5aebaf4862
qudrnr102403 <qudrnr102403@gmail.com>
2020-06-26 21:32:59 +0900

    Revert "Revert "Update FoodImages.java file""
    This reverts commit 3a4fc845e6c840bbfd91425ccc683daf9574d0b1.

--- OSS_Project/src/FoodImages/FoodImages.java
+++ OSS_Project/src/FoodImages/FoodImages.java
@@ -3,9 +3,8 @@ package FoodImages;
 public class FoodImages {
 	
 	private int ImageID; //이미지 ID
-	private int width; //가로 길이
-	private int height; //세로 길이
-	private int filesize; //이미지 파일 크기
+	private String FilePath; // 이미지 경로
+	
 	
 	public int getImageID() {
 		return ImageID;
@@ -13,23 +12,12 @@ public class FoodImages {
 	public void setImageID(int imageID) {
 		ImageID = imageID;
 	}
-	public int getWidth() {
-		return width;
+	public String getFilePath() {
+		return FilePath;
 	}
-	public void setWidth(int width) {
-		this.width = width;
-	}
-	public int getHeight() {
-		return height;
-	}
-	public void setHeight(int height) {
-		this.height = height;
-	}
-	public int getFilesize() {
-		return filesize;
-	}
-	public void setFilesize(int filesize) {
-		this.filesize = filesize;
+	public void setFilePath(String filePath) {
+		FilePath = filePath;
 	}
 	
+
 }
