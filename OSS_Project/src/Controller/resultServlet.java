739f30881119272aea2c8b60e120c6a5a9cc64b6
kiziri <wkwlwkwlwh@gmail.com>
2020-06-28 21:42:07 +0900

    Update resultServlet.java

--- OSS_Project/src/Controller/resultServlet.java
+++ OSS_Project/src/Controller/resultServlet.java
@@ -1,11 +1,14 @@
 package Controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Service.ResultService;
 
 
 @WebServlet("/result.do")
@@ -14,11 +17,15 @@ public class resultServlet extends HttpServlet {
        
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8");
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		ResultService rService = new ResultService();
 		
 		try {
-			
+			request.setCharacterEncoding("utf-8");
+			String foodName = request.getParameter("foodName");
+			request.setAttribute("list", rService.select(foodName));
+			request.getRequestDispatcher("/result.jsp").forward(request, response);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
