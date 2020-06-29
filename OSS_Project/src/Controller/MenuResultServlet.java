d285f440e9b346b506085f049e4a69a70d217606
CBK107 <55549629+CBK107@users.noreply.github.com>
2020-06-29 23:47:25 +0900

    Update MenuResultServlet.java

--- OSS_Project/src/Controller/MenuResultServlet.java
+++ OSS_Project/src/Controller/MenuResultServlet.java
@@ -7,16 +7,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Service.MenuService;
+
 
 @WebServlet("/MenuResult.do")
 public class MenuResultServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String category = null;
     
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+			MenuService cService = new MenuService();
+		
 		try {
-			
+			request.setCharacterEncoding("utf-8");
+			String taste = request.getParameter("Taste");
+			String cat = request.getParameter("Category");
+			request.setAttribute("list", cService.suggest(taste, category));
+			request.getRequestDispatcher("/MenuResult.do").forward(request, response);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
