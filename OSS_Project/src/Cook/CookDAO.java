b28e9c56347e241f10dfa452a54e0ebfd11b8670
CBK107 <55549629+CBK107@users.noreply.github.com>
2020-06-29 23:47:36 +0900

    Update CookDAO.java

--- OSS_Project/src/Cook/CookDAO.java
+++ OSS_Project/src/Cook/CookDAO.java
@@ -11,21 +11,28 @@ public class CookDAO {
 	//재료, 레시피, 맛 아이디 가져오기(외래키)
 	public ArrayList<Cook> suggest(String taste, String cat) throws Exception{
 		Connection con =null;
-		PreparedStatement stmt = null;
+		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = "";
+		String cookSql = "SELECT FoodName, FilePath\r\n" + 
+				"FROM food f, foodimage i, taste t, category c\r\n" + 
+				"WHERE i.ImageID = f.ImageID AND f.TasteID = t.TasteID AND f.CategoryID = c.CategoryID \r\n" + 
+				"AND t.TasteName = ? AND c.CategoryName = ?\r\n" + 
+				"ORDER BY rand() limit 3;";
 		
 		ArrayList<Cook> list = new ArrayList<Cook>();
 		
 		try {
 			con = DBManager.getConnection();
-			stmt = con.prepareStatement(sql);
-			rs = stmt.executeQuery();
+			pstmt = con.prepareStatement(cookSql);
+			pstmt.setString(1, taste);
+			pstmt.setString(2, cat);
+			rs = pstmt.executeQuery();
 			while (rs.next()) {
+				list.add(new Cook(rs.getString(1), rs.getString(2)));
 			}
 		}
 		finally {
-			DBManager.close(rs, stmt, con);
+			DBManager.close(rs, pstmt, con);
 		}
 		
 		return list;
