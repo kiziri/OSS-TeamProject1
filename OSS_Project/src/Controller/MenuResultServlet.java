76c9a6bd6f56aa542c778a735a99ffa1c59fcfdd
kiziri <wkwlwkwlwh@gmail.com>
2020-06-30 01:31:52 +0900

    Update MenuResultServlet.java

--- OSS_Project/src/Controller/MenuResultServlet.java
+++ OSS_Project/src/Controller/MenuResultServlet.java
@@ -24,7 +24,6 @@ public class MenuResultServlet extends HttpServlet {
 			request.setCharacterEncoding("utf-8");
 			String cat = request.getParameter("Category");
 			String taste = request.getParameter("Taste");
-			System.out.println(taste + " " + cat);
 			request.setAttribute("list", cService.suggest(taste, cat));
 			request.getRequestDispatcher("/MenuResult.jsp").forward(request, response);
 		}
