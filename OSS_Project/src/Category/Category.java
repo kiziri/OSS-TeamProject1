f08858c897b579ba838f17d1e2d3c34545131dce
CBK107 <55549629+CBK107@users.noreply.github.com>
2020-06-29 17:21:31 +0900

    Update Category.java

--- OSS_Project/src/Category/Category.java
+++ OSS_Project/src/Category/Category.java
@@ -2,27 +2,32 @@ package Category;
 
 public class Category {
 	
-	private int CategoryID; //카테고리 ID
-	private String CategoryName; //카테고리 이름(중식, 한식, 일식, 양식)
+	private int categoryId; //카테고리 ID
+	private String categoryName; //카테고리 이름(중식, 한식, 일식, 양식)
 	
-	public Category(int categoryID, String categoryName) {
+	
+	
+	public Category(int categoryId, String categoryName) {
 		super();
-		CategoryID = categoryID;
-		CategoryName = categoryName;
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
 	}
 	
-	public int getCategoryID() {
-		return CategoryID;
+	
+	public int getCategoryId() {
+		return categoryId;
 	}
-	public void setCategoryID(int categoryID) {
-		CategoryID = categoryID;
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
 	}
 	public String getCategoryName() {
-		return CategoryName;
+		return categoryName;
 	}
 	public void setCategoryName(String categoryName) {
-		CategoryName = categoryName;
+		this.categoryName = categoryName;
 	}
+	
+	
 	
 	
 }
