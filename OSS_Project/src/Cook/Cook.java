b54f361a17fb5f5d44c59d30049501a555796fff
CBK107 <55549629+CBK107@users.noreply.github.com>
2020-06-29 17:21:34 +0900

    Update Cook.java

--- OSS_Project/src/Cook/Cook.java
+++ OSS_Project/src/Cook/Cook.java
@@ -2,34 +2,36 @@ package Cook;
 
 public class Cook {
 	
-	private int IngredientsID; //식재료 ID(외래키)
-	private int RecipeID; //레시피 ID(외래키)
-	private int Taste_ID; //맛 ID(외래키)
+	private int ingredientsId; //식재료 ID(외래키)
+	private int recipeId; //레시피 ID(외래키)
+	private int taste_Id; //맛 ID(외래키)
+	
+	public Cook(int ingredientsId, int recipeId, int taste_Id) {
+		super();
+		this.ingredientsId = ingredientsId;
+		this.recipeId = recipeId;
+		this.taste_Id = taste_Id;
+	}
+	
+	public int getIngredientsId() {
+		return ingredientsId;
+	}
+	public void setIngredientsId(int ingredientsId) {
+		this.ingredientsId = ingredientsId;
+	}
+	public int getRecipeId() {
+		return recipeId;
+	}
+	public void setRecipeId(int recipeId) {
+		this.recipeId = recipeId;
+	}
+	public int getTaste_Id() {
+		return taste_Id;
+	}
+	public void setTaste_Id(int taste_Id) {
+		this.taste_Id = taste_Id;
+	}
 
 	
-	public Cook(int ingredientsID, int recipeID, int taste_ID) {
-		super();
-		IngredientsID = ingredientsID;
-		RecipeID = recipeID;
-		Taste_ID = taste_ID;
-	}
 	
-	public int getTasteID() {
-		return Taste_ID;
-	}
-	public void setTasteID(int tasteID) {
-		Taste_ID = tasteID;
-	}
-	public int getIngredientsID() {
-		return IngredientsID;
-	}
-	public void setIngredientsID(int ingredientsID) {
-		IngredientsID = ingredientsID;
-	}
-	public int getRecipeID() {
-		return RecipeID;
-	}
-	public void setRecipeID(int recipeID) {
-		RecipeID = recipeID;
-	}
 }
