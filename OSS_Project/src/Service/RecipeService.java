744edf86b272a26c3ff1d7006bb74f9d07630ba3
kiziri <wkwlwkwlwh@gmail.com>
2020-06-30 18:35:26 +0900

    Update RecipeService.java

--- OSS_Project/src/Service/RecipeService.java
+++ OSS_Project/src/Service/RecipeService.java
@@ -8,8 +8,11 @@ import Recipe.RecipeDAO;
 public class RecipeService {
 	RecipeDAO rDao = new RecipeDAO();
 	
-	public ArrayList<Recipe> recommend(String ingredients1, String ingredients2, String ingredients3, String ingredients4, String ingredients5) throws Exception {
-		return rDao.recommend(ingredients1, ingredients2, ingredients3, ingredients4, ingredients5);
+	public ArrayList<Recipe> recommend(String ingredients1, String ingredients2, String ingredients3, 
+										String ingredients4, String ingredients5, String ingredients6, 
+										String ingredients7, String ingredients8) throws Exception {
 		
+		return rDao.recommend(ingredients1, ingredients2, ingredients3, ingredients4, 
+								ingredients5, ingredients6, ingredients7, ingredients8);
 	}
 }
